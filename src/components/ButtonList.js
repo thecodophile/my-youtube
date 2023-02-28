c8218ff52a3808c0caea610266fea91b287e2916
Somnath Dey <sdey.aklakhi@gmail.com>
2023-02-28 20:15:02 +0530

    fetch api and fill videoContainer with live data

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,7 +1,28 @@
 import React from "react";
+import Button from "./Button";
 
 const ButtonList = () => {
-  return <div>ButtonList</div>;
+  const button_list = [
+    "All",
+    "Gaming",
+    "Songs",
+    "Live",
+    "Cricket",
+    "React",
+    "News",
+    "Live",
+    "Cricket",
+    "React",
+    "News",
+  ];
+
+  return (
+    <div className="flex">
+      {button_list.map((value, index) => {
+        return <Button name={value} key={index} />;
+      })}
+    </div>
+  );
 };
 
 export default ButtonList;
---
+++ src/components/VideoCard.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const VideoCard = ({ info }) => {
+  console.log(info);
+  const { snippet, statistics } = info;
+  const { channelTitle, title, thumbnails } = snippet;
+  return (
+    <div className="p-2 m-2 w-64 shadow-lg">
+      <img src={thumbnails.medium.url} alt="thumbnail" className="rounded-lg" />
+      <ul>
+        <li className="font-bold py-2">{title}</li>
+        <li>{channelTitle}</li>
+        <li>{statistics.viewCount} views</li>
+      </ul>
+    </div>
+  );
+};
+
+export default VideoCard;
